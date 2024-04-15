4498593055604ab662c54323abe68504bab9ba3e
Martin Williams <martinwork@users.noreply.github.com>
2024-04-15 16:03:09 +0100

    Add shim

--- test.ts
+++ test.ts
@@ -1,1 +1,14 @@
-// tests go here; this will not be compiled when this package is used as an extension.
+
+function test( str: string, num: number) {
+    serial.writeString(str + " = ")
+    serial.writeNumber(num)
+    serial.writeLine("")
+}
+
+input.onButtonPressed(Button.A, function () {
+    test("Math.power(10, 3 / 2)", 				Math.power(10, 3 / 2))
+    test("Math.power(-1.4142135623730951, -2)", Math.power(-1.4142135623730951, -2))
+    test("Math.power(-1, 3 / 2)", 				Math.power(-1, 3 / 2))
+    test("Math.power(0, 1)", 					Math.power(0, 1))
+    test("Math.power(0, -1)", 					Math.power(0, -1))
+})
